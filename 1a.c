ac1bdcb006e2625508d6071d083066867a1c935c
route-2 <ruthurao@gmail.com>
2022-08-04 20:32:19 +0530

    comment

--- 1a.c
+++ 1a.c
@@ -9,40 +9,45 @@ int main()
 {
     char buffer[256],temp[10];
     int n;
-    printf("enter the no of frames to be sent");
+    printf("enter the no of frames to be sent"); // takes users input for no of frames to be sent
     scanf("%d",&n);
 
-    for(int i=0;i<n;i++)
+    for(int i=0;i<n;i++) // prints the frames and stores in buffer
     
     {
-        bzero(buffer,256);
+        bzero(buffer,256); //turns buffer to 0s
         printf("enter the frames at %d",(i+1));
         scanf("%s",buffer);
+        //buffer of type string
 
-        int len = strlen(buffer);
+        int len = strlen(buffer);//length of the buffer
         printf("the length of the frames are %d",len);
-        sprintf(temp,"%d",len);
-        strcat(frames,temp);
-        strcat(frames,buffer);
+        sprintf(temp,"%d",len); // store len in temp of type int
+        strcat(frames,temp); // concatenate temp to frames
+        strcat(frames,buffer); // concatenate buffer to frames
     }
     reciver();
 }
 void reciver()
 {
     printf("\n the frame recived is %s",frames);
+    //print frames
+
     int frameslen = strlen(frames);
     printf("\n the length of the frame is %d",frameslen);
+    //prints frames len after strlen
 
     int i = 0;
     while(i<frameslen)
     {
         char leninchar=frames[i++];
-        int len = (int)leninchar- (int)'0';
+        int len = (int)leninchar- (int)'0'; //converts char to int
         while(len>0)
         {
             printf("%c",frames[i++]);
             len--;
         }
         printf("\n");
+        //prints char by char
             }
 }
